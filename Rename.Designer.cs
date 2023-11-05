6d0667d16078da4674a8a9d9c818da72bf3f5170
Jhonatan <jhonatan.vitoria@hotmail.com>
2023-11-05 14:55:10 -0300

    first commit

---
+++ Rename.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Rename
+{
+    partial class Rename
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rename));
+            this.btnIniciarRenomeacao = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tutorialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnIniciarRenomeacao
+            // 
+            this.btnIniciarRenomeacao.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnIniciarRenomeacao, "btnIniciarRenomeacao");
+            this.btnIniciarRenomeacao.Name = "btnIniciarRenomeacao";
+            this.btnIniciarRenomeacao.UseVisualStyleBackColor = true;
+            this.btnIniciarRenomeacao.Click += new System.EventHandler(this.btnIniciarRenomeacao_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tutorialToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            resources.ApplyResources(this.menuToolStripMenuItem, "menuToolStripMenuItem");
+            // 
+            // tutorialToolStripMenuItem
+            // 
+            this.tutorialToolStripMenuItem.BackColor = System.Drawing.SystemColors.Window;
+            this.tutorialToolStripMenuItem.Name = "tutorialToolStripMenuItem";
+            resources.ApplyResources(this.tutorialToolStripMenuItem, "tutorialToolStripMenuItem");
+            this.tutorialToolStripMenuItem.Click += new System.EventHandler(this.Tutorial);
+            // 
+            // Rename
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.btnIniciarRenomeacao);
+            this.Controls.Add(this.menuStrip1);
+            this.HelpButton = true;
+            this.Name = "Rename";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciarRenomeacao;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tutorialToolStripMenuItem;
+    }
+}
+
---
+++ Rename.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using ExcelDataReader;
+
+namespace Rename
+{
+    public partial class Rename : Form
+    {
+        public Rename()
+        {
+            InitializeComponent();
+        }
+
+        //botão de renomeio
+        private void btnIniciarRenomeacao_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                folderBrowserDialog.Description = "Selecione a pasta que contém a planilha Excel";
+
+                if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("Nenhuma pasta selecionada.");
+                    return;
+                }
+
+                string pastaOrigem = folderBrowserDialog.SelectedPath;
+                string[] arquivosExcel = Directory.GetFiles(pastaOrigem, "*.xls*");
+
+                string filePath = ProcurarPlanilhaCorrespondente(arquivosExcel);
+
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("Nenhuma planilha correspondente encontrada.");
+                    return;
+                }
+
+                Dictionary<string, string> mapeamentoNomeNovo = LerPlanilhaExcel(filePath);
+
+                string pastaDestino = pastaOrigem;
+
+                RenomearPastasEArquivos(pastaDestino, mapeamentoNomeNovo);
+
+                MessageBox.Show("Renomeação concluída.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+
+
+        //padrão exemplo de qual panilha sera pego as informações para renomeio
+        private string ProcurarPlanilhaCorrespondente(string[] arquivosExcel)
+        {
+            string pattern = @"\d{2}-\d{5}\.xls[x]?";
+
+            foreach (string arquivo in arquivosExcel)
+            {
+                string nomeArquivo = Path.GetFileName(arquivo);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(nomeArquivo, pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    return arquivo;
+                }
+            }
+
+            return null;
+        }
+
+        //seleção das colunas a serem usadas da planilha
+        private Dictionary<string, string> LerPlanilhaExcel(string filePath)
+        {
+            Dictionary<string, string> mapeamentoNomeNovo = new Dictionary<string, string>();
+
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.FieldCount >= 12)
+                        {
+                            string nomeAntigo = reader.GetString(0);
+                            if (reader.FieldCount > 11)
+                            {
+                              object valorColuna12 = reader.GetValue(11);
+
+                                if (valorColuna12 != null)
+                                {
+                                     string novoNome = nomeAntigo + " Rev." + valorColuna12.ToString();
+                                     mapeamentoNomeNovo[nomeAntigo] = novoNome;
+                                }  
+                            }
+
+                        }
+                    }
+                } while (reader.NextResult());
+            }
+
+            return mapeamentoNomeNovo;
+        }
+
+        //renomeação dos documentos
+        private void RenomearPastasEArquivos(string pastaDestino, Dictionary<string, string> mapeamentoNomeNovo)
+        {
+            // Renomear pastas
+            foreach (string subdiretorio in Directory.GetDirectories(pastaDestino))
+            {
+                string nomePasta = new DirectoryInfo(subdiretorio).Name;
+
+                if (mapeamentoNomeNovo.TryGetValue(nomePasta, out string novoNomePasta))
+                {
+                    string novoCaminhoPasta = Path.Combine(pastaDestino, novoNomePasta);
+                    Directory.Move(subdiretorio, novoCaminhoPasta);
+                }
+            }
+
+            // Renomear arquivos
+            foreach (string filePath in Directory.GetFiles(pastaDestino))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                string fileExtension = Path.GetExtension(filePath);
+
+                if (mapeamentoNomeNovo.TryGetValue(fileName, out string novoNome))
+                {
+                    string novoCaminho = Path.Combine(pastaDestino, novoNome + fileExtension);
+                    File.Move(filePath, novoCaminho);
+                }
+            }
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Rename());
+        }
+
+        private void ajudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tutorial(object sender, EventArgs e)
+        {
+            Tutorial tutorialForm = new Tutorial();
+            tutorialForm.ShowDialog();
+        }
+    }
+}
